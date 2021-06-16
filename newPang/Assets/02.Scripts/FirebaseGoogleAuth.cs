e3b1f994fd60c7fe489e1c153b4d852a2326a0de
ChanHyeong <65081817+ych7305@users.noreply.github.com>
2021-06-16 21:23:19 +0900

    ui 작업 중

---
+++ newPang/Assets/02.Scripts/FirebaseGoogleAuth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class FirebaseGoogleAuth : MonoBehaviour
+{
+    public Text email;
+
+    void Start()
+    {
+        PlayGamesPlatform.InitializeInstance(
+ new PlayGamesClientConfiguration.Builder().Build());
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        // Google Play Games 활성화
+    }
+
+
+    public void TryGoogleLogin()
+    {
+        if (!Social.localUser.authenticated) // 로그인 되어 있지 않다면
+        {
+            Social.localUser.Authenticate(success => // 로그인 시도
+            {
+                if (success) // 성공하면
+                {
+                    email.text = Social.localUser.userName;
+                }
+                else // 실패하면
+                {
+                    email.text = "Fail...";
+                }
+            });
+        }
+    }
+
+
+    public void TryGoogleLogout()
+    {
+        if (Social.localUser.authenticated) // 로그인 되어 있다면
+        {
+            PlayGamesPlatform.Instance.SignOut(); // 로그아웃
+            email.text = "Logout";
+        }
+    }
+}
---
+++ newPang/Assets/02.Scripts/Manager/MainLobbySceneManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLobbySceneManager : SingletonClass<MainLobbySceneManager>
+{
+    [SerializeField] GameObject mainView;
+    [SerializeField] GameObject characterView;
+
+    [SerializeField] GameObject loadingPopip;
+
+    Stack<GameObject> uiStack;
+
+
+    protected override void Awake()
+    {
+        uiStack = new Stack<GameObject>();
+    }
+    public void Popup(GameObject popup)
+    {
+        popup.SetActive(true);
+        uiStack.Push(popup);
+    }
+
+    public void ClosePopup()
+    {
+        if (uiStack.Count > 0)
+        {
+            uiStack.Pop().SetActive(false);
+        }
+    }
+
+    public void PVPMatching()
+    {
+        ClosePopup();
+        Popup(loadingPopip);
+        LobbyManager.GetInstance.JoinNormalRoomWithCheckPhoton();
+    }
+    public void CoOpMatching()
+    {
+        ClosePopup();
+        Popup(loadingPopip);
+        LobbyManager.GetInstance.JoinCoOpRoomWithCheckPhoton();
+    }
+
+    //public void SingleGame()
+    //{
+    //    LobbyManager.GetInstance.JoinSingleRoomWithCheckPhoton();
+    //}
+
+    public void MatchingCancel()
+    {
+        LobbyManager.GetInstance.MatchingCancel();
+    }
+
+    public void OnCharacterView()
+    {
+        mainView.SetActive(false);
+        characterView.SetActive(true);
+    }
+
+    public void ExitCharacterView()
+    {
+        characterView.SetActive(false);
+        mainView.SetActive(true);
+    }
+}
